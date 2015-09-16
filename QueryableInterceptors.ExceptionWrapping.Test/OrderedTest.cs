e2da4c8e9732bfeb0f27429a8e73361aa6534af8
Beau Trepp <beau.trepp@riotinto.com>
2015-09-16 16:00:55 +0800

    Adds tests for OrderedQueryables
    A simple test case that tests if ordered by is callable on the queryable.
    Queryables will throw unable to cast exceptions if it is not supported

---
+++ QueryableInterceptors.ExceptionWrapping.Test/OrderedTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace QueryableInterceptors.ExceptionWrapping.Test
+{
+    [TestFixture]
+    public class OrderedTest
+    {
+        IQueryable<object> _items;
+        [SetUp]
+        public void Setup()
+        {
+            _items = Enumerable.Empty<object>()
+                        .AsQueryable()
+                        .WrapExceptions()
+                        .OfType<Exception>()
+                        .With<Exception>();
+        }
+
+        [Test]
+        public void OrderByOnInterceptedQueryable()
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                var result = _items.OrderBy(s => s.GetHashCode());
+            });
+        }
+    }
+}
